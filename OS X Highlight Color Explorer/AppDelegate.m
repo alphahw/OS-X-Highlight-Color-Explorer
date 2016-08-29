903a60bb2225a7c306f6d8f7bee347c805908fe8
Albin Halinen Wilén <albin_wilen@msn.com>
2016-08-28 23:10:02 -0400

    Highlight color extraction + display

--- OS X Highlight Color Explorer/AppDelegate.m
+++ OS X Highlight Color Explorer/AppDelegate.m
@@ -11,12 +11,27 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
+@property (weak) IBOutlet NSColorWell *colorWell;
+@property (weak) IBOutlet NSTextField *textField;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+
+    NSColor *highlightColor = [[NSColor selectedControlColor] colorUsingColorSpace:[NSColorSpace sRGBColorSpace]];
+    int rgbMax = 255;
+
+    [_colorWell setColor:highlightColor];
+    [_textField setStringValue:[NSString stringWithFormat:@"R: %.f\nG: %.f\nB: %.f\nA: %.1f",
+                                highlightColor.redComponent * rgbMax,
+                                highlightColor.greenComponent * rgbMax,
+                                highlightColor.blueComponent * rgbMax,
+                                highlightColor.alphaComponent]];
+
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
